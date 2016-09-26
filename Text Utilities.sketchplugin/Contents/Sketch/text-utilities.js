7ae30bc11866790fd97e10a1825d665497d0b67e
Sam Deane <sam@elegantchaos.com>
2016-09-26 18:03:11 +0100

    Starting adding some literate/descriptive comments for docco.

--- Text Utilities.sketchplugin/Contents/Sketch/text-utilities.js
+++ Text Utilities.sketchplugin/Contents/Sketch/text-utilities.js
@@ -1,3 +1,104 @@
+// Text Utilities, by Johnnie Walker — Source code available at [GitHub](https://github.com/BohemianCoding/plugins.examples.text-utilities)
+//
+// This plugin illustrates a few techniques:
+// - iterating over the selected layers
+// - analysing the text layout (in great detail!)
+// - creating new layers
+// - defining multiple commands in a single plugin
+//
+// ## Layout Of The Plugin
+//
+// The first thing to do when making a Plugin is to setup the folder structure, which should
+// look something like this:
+//
+// ```
+//    MyPlugin.sketchplugin/
+//      Contents/
+//        Sketch/
+//          manifest.json
+//          script.js
+//        Resources/
+//          a-resource.png
+//          other-resource.txt
+// ```
+//
+// For a Plugin with bundled resources, you want to add a `Resources` folder as well
+// as the normal `Sketch` folder, and you want to drop into it any resources that your
+// script needs to access. These can be files of any type (but obviously keep in mind
+// that they contribute to the overall size of the Plugin, so your users won't thank
+// you if you put too much stuff in there).
+//
+// ## Manifest
+//
+// The Plugin needs a `manifest.json` file. This tells Sketch which menu items your Plugin supplies,
+// as well as giving some general information about the Plugin such as its name, author, and so on.
+//
+// A single Plugin can supply multiple menu items, and each one can execute different code,
+// or they can all share code. In our case though, we just have one command.
+//
+//  ```json
+// {
+//   "author" : "Johnnie Walker",
+//   "commands" : [
+//     {
+//       "script" : "text-utilities.js",
+//       "handler" : "onAddBoth",
+//       "name" : "Add Line Fragments & Baselines",
+//       "identifier" : "fragmentsandbaselines"
+//     },
+//     {
+//       "script" : "text-utilities.js",
+//       "handler" : "onAddBaselines",
+//       "name" : "Add Baselines",
+//       "identifier" : "baselines"
+//     },
+//     {
+//       "script" : "text-utilities.js",
+//       "handler" : "onAddLineFragments",
+//       "name" : "Add Line Fragments",
+//       "identifier" : "fragments"
+//     },
+//     {
+//       "script" : "text-utilities.js",
+//       "handler" : "onUseLegacyBaselines",
+//       "name" : "Use legacy typesetter",
+//       "identifier" : "legacyTypesetter"
+//     },
+//     {
+//       "script" : "text-utilities.js",
+//       "handler" : "onUseConstantBaselines",
+//       "name" : "Use constant baselines typesetter",
+//       "identifier" : "constantBaselinesTypesetter"
+//     },
+//   ],
+//   "menu": {
+//     "items": [
+//       "baselines",
+//       "fragments",
+//       "fragmentsandbaselines",
+//       "-",
+//       "legacyTypesetter",
+//       "constantBaselinesTypesetter",
+//     ],
+//   },
+//   "identifier" : "com.sketchapp.examples.text-utilities",
+//   "version" : "2.0",
+//   "description" : "Utilities for text layers",
+//   "authorEmail" : "developer@sketchapp.com",
+//   "name" : "Text Layer Utilities"
+// }
+// ```
+
+// ## Code
+// ### Defining The Run Handler
+
+// In the manifest, we told Sketch that every time the "Bundled Resources Example" menu is selected,
+// we want to execute a javascript handler called `onRun` in the `resources.js` file.
+
+// So now we need to put some code into the `resources.js` file to implement that command.
+
+
+
 var fillInRect = function(container, rect, color) {
   if (! color) {
     color = [MSColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.6];
@@ -77,6 +178,23 @@ var addLineFragments = function(sketch, container, fragments) {
 }
 
 
+var setTypeSetterMode = function(context, lineSpacingBehaviour) {
+    var sketch = context.api()
+    sketch.selectedDocument.selectedLayers.iterate(filter = "isText", function(layer) {
+        var textLayer = layer._object;
+        var initialBaselineOffset = textLayer.firstBaselineOffset();
+        textLayer.lineSpacingBehaviour = lineSpacingBehaviour;
+        var baselineOffset = textLayer.firstBaselineOffset();
+
+        var rect = layer.frame;
+        rect.y -= (baselineOffset - initialBaselineOffset);
+        layer.frame = rect;
+        sketch.log("Set spacing mode for '" + layer.name + "' to " + lineSpacingBehaviour)
+    })
+}
+
+
+
 var onAddLineFragments = function(context) {
     var sketch = context.api()
     sketch.selectedDocument.selectedLayers.iterate(function(layer) {
@@ -103,21 +221,6 @@ var onAddBoth = function(context) {
     }, "isText")
 };
 
-
-var setTypeSetterMode = function(context, lineSpacingBehaviour) {
-    var sketch = context.api()
-    sketch.selectedDocument.selectedLayers.iterate(filter = "isText", function(layer) {
-        var textLayer = layer._object;
-        var initialBaselineOffset = textLayer.firstBaselineOffset();
-        textLayer.lineSpacingBehaviour = lineSpacingBehaviour;
-        var baselineOffset = textLayer.firstBaselineOffset();
-
-        var rect = layer.frame;
-        rect.y -= (baselineOffset - initialBaselineOffset);
-        layer.frame = rect;
-        sketch.log("Set spacing mode for '" + layer.name + "' to " + lineSpacingBehaviour)
-    })
-}
 
 var onUseLegacyBaselines = function(context) {
     setTypeSetterMode(context, 1);
